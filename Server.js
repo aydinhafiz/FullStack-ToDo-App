bb4b2989d2b5c309baa256513e9e3628ba8230ac
Adyin Hafiz <ahafiz@MBP-von-admin.fritz.box>
2024-07-03 16:24:28 +0200

    fixed some bugs

--- Server.js
+++ Server.js
@@ -3,21 +3,28 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const toDoRoutes = require('./routes/ToDoRoute');
 
+// MongoDB bağlantı dizesi
+const mongoURI = 'mongodb+srv://hafizaydin01:aydin2006@todo-app.0n4i5ys.mongodb.net/todoapp?retryWrites=true&w=majority&appName=todo-app';
+
+// Express uygulamasını başlat
 const app = express();
 const port = 3000;
-
-// MongoDB bağlantısı
-mongoose.connect('mongodb://localhost:27017/todoapp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
 
 // Middleware
 app.use(bodyParser.json());
 
+// MongoDB'ye bağlan
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB connection error:', err));
+
 // Route'lar
 app.use('/api/todo', toDoRoutes);
 
+// Sunucuyu dinlemeye başla
 app.listen(port, () => {
   console.log(`Sunucu http://localhost:${port} adresinde çalışıyor`);
 });
